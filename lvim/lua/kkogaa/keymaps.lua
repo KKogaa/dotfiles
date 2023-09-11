ad7b7f0ff9bd361264af04d2616c20f937095acc
KKogaa <akoga@pucp.edu.pe>
2023-09-10 22:36:39 -0500

    Added vscode settings

--- lvim/lua/kkogaa/keymaps.lua
+++ lvim/lua/kkogaa/keymaps.lua
@@ -37,6 +37,5 @@ vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/the
 -- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+  vim.cmd("so")
 end)
-
